13283ae0526eca560a7614415fde8e207d0d6080
Shiya Bai <ssshiya0301@gmail.com>
2019-10-18 02:15:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mybuttonapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mybuttonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1 = (TextView)findViewById(R.id.textView);
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t1.setText("Ahh, You clicked me!");
+            }
+        });
+    }
+}
